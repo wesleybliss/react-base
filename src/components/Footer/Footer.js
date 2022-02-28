b4c9b067d26b2f4b2e384ae018fa6f5c34293e95
Wesley Bliss <wesley.bliss@gmail.com>
2022-02-28 14:46:57 -0500

    Misc cleanup

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,54 +2,49 @@ import cn from 'classnames'
 
 import { Link } from 'react-router-dom'
 
+import './Footer.css'
+
 const Footer = () => {
     
     return (
         
-        <div
-            className={cn(
-                'Footer',
-                'grid grid-cols-3 gap-10 items-center content-center',
-                'w-full p-6',
-                'text-gray-200',
-                'bg-gray-900',
-            )}>
+        <div className="Footer">
             
-            <div className="">
-                <h4 className="text-xl text-center font-bold">
+            <section>
+                <h4>
                     SECTION
                 </h4>
-                <div className="flex flex-col justify-start items-center">
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
+                <div>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
                 </div>
-            </div>
+            </section>
             
-            <div className="">
-                <h4 className="text-xl text-center font-bold">
+            <section>
+                <h4>
                     SECTION
                 </h4>
-                <div className="flex flex-col justify-center items-center">
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
+                <div>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
                 </div>
-            </div>
+            </section>
             
-            <div className="">
-                <h4 className="text-xl text-center font-bold">
+            <section>
+                <h4>
                     SECTION
                 </h4>
-                <div className="flex flex-col justify-end items-center">
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
-                    <Link className="" to="/">ITEM</Link>
+                <div>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
+                    <Link to="/">ITEM</Link>
                 </div>
-            </div>
+            </section>
             
         </div>
         
